bf650920525f618e4e3c0a0d41a0c6e0e47265bd
George Doykan <George Doykan>
2018-04-23 16:09:43 -0500

    able to load mutiple pages

--- Scraper.py
+++ Scraper.py
@@ -18,27 +18,34 @@ positions = []
 heights = []
 weights = []
 
-# #grabs all names
-# divs = driver.find_elements_by_xpath('//div[contains(@class, "name")]//a')
-# for div in divs:
-# 	names.append(div.text)
-# 	print div.text
+#load x pages of rankings 
+element_present = EC.presence_of_element_located((By.XPATH, '//a[contains(@data-js, "showmore")]'))
 
-# #grabs all hometowns
-# homes = driver.find_elements_by_xpath('//div[contains(@class, "name")]//span')
-# for home in homes:
-# 	hometowns.append(home.text)
-# 	print home.text
+for x in range (0,6):
+	driver.find_elements_by_xpath('//a[contains(@data-js, "showmore")]')[0].click()
+	WebDriverWait(driver, timeout).until(element_present) #wait until web page is loaded
 
-#grab heights
-# pos = driver.find_elements_by_xpath('//ul[contains(@class, "metrics-list")]//li[contains(@class, "position")]')
-# for po in pos:
-# 	positions.append(pos.text)
+#grabs all names
+divs = driver.find_elements_by_xpath('//div[contains(@class, "name")]//a')
+for div in divs:
+	names.append(div.text)
+	print div.text
 
-# ht = driver.find_elements_by_xpath('//ul[contains(@class, "metrics-list")]//li[contains(@class, "height")]') 
-# for h in ht:
-# 	heights.append(h.text)
+#grabs all hometowns
+homes = driver.find_elements_by_xpath('//div[contains(@class, "name")]//span')
+for home in homes:
+	hometowns.append(home.text)
+	print home.text
 
-# wt = driver.find_elements_by_xpath('//ul[contains(@class, "metrics-list")]//li[contains(@class, "weight")]') 
-# for w in wt:
-# 	weights.append(w.text)
+grab heights
+pos = driver.find_elements_by_xpath('//ul[contains(@class, "metrics-list")]//li[contains(@class, "position")]')
+for po in pos:
+	positions.append(pos.text)
+
+ht = driver.find_elements_by_xpath('//ul[contains(@class, "metrics-list")]//li[contains(@class, "height")]') 
+for h in ht:
+	heights.append(h.text)
+
+wt = driver.find_elements_by_xpath('//ul[contains(@class, "metrics-list")]//li[contains(@class, "weight")]') 
+for w in wt:
+	weights.append(w.text)
